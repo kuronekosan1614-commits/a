0297114c363e187fca4e6c71cee65833f5f29c93
Juanes <kuronekosan1614@gmail.com>
2025-08-25 14:36:54 -0500

    Configuro WhiteNoise y staticfiles

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -134,6 +134,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
